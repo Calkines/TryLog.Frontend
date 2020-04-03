6a75ce298582f4a72755fdbbf4bc0d94bf7ad59b
Rafael CP <rafael_it@hotmail.com>
2020-04-03 17:47:00 -0300

    chore: remove UserInformation component e troca div por v-container

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="ml-5 mr-5">
-    <UserInformation class="mt-5" />
-
+  <v-container>
     <h1 class="mt-5 text-center">Dashboard</h1>
 
     <!-- Table -->
     <DashboardTableLog />
-  </div>
+  </v-container>
 </template>
 <script>
 import DashboardTableLog from "../components/DashboardTableLog";
-import UserInformation from "../components/UserInformation";
 
 export default {
   name: "Dashboard",
   components: {
-    DashboardTableLog,
-    UserInformation
+    DashboardTableLog
   },
   data() {
     return {};
