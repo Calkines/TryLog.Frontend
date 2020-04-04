d5100cdb85f7213f0bc12d942a3fa6bf9d7f4ccf
Rafael CP <rafael_it@hotmail.com>
2020-04-04 20:37:26 -0300

    feat: add componente notfound 404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col class="d-flex justify-center align-self-start">
+        <span class="display-1 text-center">
+          Ops! Essa página não pode ser encontrada.
+        </span>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
