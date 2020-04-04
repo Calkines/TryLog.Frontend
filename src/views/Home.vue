03c12810cddf79eb5b37b18b6f5112816821d234
Rafael CP <rafael_it@hotmail.com>
2020-04-04 17:00:48 -0300

    refact: altera nome de compoentes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,27 @@
   <div class="home">
     <v-container>
       <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-      <h1>Bem vindo</h1>
+
+      <h1 class="text-center">Resumo das operações</h1>
+
+      <v-row>
+        <v-col
+          v-for="statistic in statistics"
+          :key="`${statistic.title}`"
+          cols="6"
+          sm="4"
+          md="3"
+          lg="3"
+        >
+          <StatisticCard :statistic="statistic" />
+        </v-col>
+      </v-row>
+
+      <v-row class="mt-5">
+        <v-col v-for="erro in erros" :key="`${erro.title}`" cols="12" md="4">
+          <ErrosGraph :erro="erro" />
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
@@ -10,11 +30,23 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
+import ErrosGraph from "../components/ErrosGraph";
+import StatisticCard from "../components/StatisticCard";
+
+import errosData from "../data/erros.json";
+import statisticsData from "../data/estatisticas.json";
 
 export default {
   name: "Home",
   components: {
-    // HelloWorld
+    ErrosGraph,
+    StatisticCard
+  },
+  data() {
+    return {
+      erros: errosData,
+      statistics: statisticsData
+    };
   }
 };
 </script>
