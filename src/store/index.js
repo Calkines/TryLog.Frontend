9713082e942482105c0a726e3828ffd7f97d3f11
Rafael CP <rafael_it@hotmail.com>
2020-04-06 12:02:22 -0300

    feat: add notification, refact statistic to a new module

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,18 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import api from "../services/ApiService";
-import * as error from "../store/modules/errors";
-import * as log from "../store/modules/logs";
+
+import error from "../store/modules/errors";
+import log from "../store/modules/logs";
+import statistic from "../store/modules/statistics";
+import * as notification from "@/store/modules/notification";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    statistics: []
-  },
-  mutations: {
-    SET_STATISTICS(state, statistics) {
-      state.statistics = statistics;
-    }
-  },
-  actions: {
-    fetchStatistics({ commit }) {
-      api
-        .getStatistics()
-        .then(r => {
-          commit("SET_STATISTICS", r.data);
-        })
-        .catch(er => {
-          console.log("An error has ocurred: ", er.response);
-        });
-    }
-  },
-  getters: {},
   modules: {
     error,
-    log
+    log,
+    statistic,
+    notification
   }
 });
