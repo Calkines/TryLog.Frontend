3b88e87a755fe21f387ce1a20a89fb0daf24dae5
Rafael CP <rafael_it@hotmail.com>
2020-04-06 12:04:10 -0300

    refact: remove console log to use notification

--- src/store/modules/logs.js
+++ src/store/modules/logs.js
@@ -1,41 +1,52 @@
 import api from "@/services/ApiService";
 
-export const state = {
-  logs: [],
-  logsTotalPages: 0
-};
-
-export const mutations = {
-  SET_LOGS(state, logs) {
-    state.logs = logs;
+export default {
+  state: {
+    logs: [],
+    logsTotalPages: 0
   },
-  SET_LOGS_TOTAL_PAGES(state, totalPages) {
-    state.logsTotalPages = totalPages;
-  }
-};
-export const actions = {
-  fetchLogs({ commit }) {
-    api
-      .getLogs()
-      .then(r => {
-        commit("SET_LOGS", r.data);
-      })
-      .catch(er => {
-        console.log("An error has ocurred: ", er.response);
-      });
+  mutations: {
+    SET_LOGS(state, logs) {
+      state.logs = logs;
+    },
+    SET_LOGS_TOTAL_PAGES(state, totalPages) {
+      state.logsTotalPages = totalPages;
+    }
   },
-  fetchLogs({ commit }, { itemsPerPage, startPage }) {
-    api
-      .getLogs(itemsPerPage, startPage)
-      .then(r => {
-        let totalItens = r.headers["x-total-count"] ?? 0;
-        let totalPage = Math.ceil(totalItens / itemsPerPage);
-        commit("SET_LOGS_TOTAL_PAGES", totalPage);
-        commit("SET_LOGS", r.data);
-      })
-      .catch(er => {
-        console.log("An error has ocurred: ", er.response);
-      });
-  }
+  actions: {
+    fetchLogs({ commit, dispatch }) {
+      api
+        .getLogs()
+        .then(r => {
+          commit("SET_LOGS", r.data);
+        })
+        .catch(er => {
+          const notification = {
+            type: "error",
+            message: "An error has ocurred",
+            detail: er
+          };
+          dispatch("notification/add", notification, { root: true });
+        });
+    },
+    fetchLogs({ commit, dispatch }, { itemsPerPage, startPage }) {
+      api
+        .getLogs(itemsPerPage, startPage)
+        .then(r => {
+          let totalItens = r.headers["x-total-count"] ?? 0;
+          let totalPage = Math.ceil(totalItens / itemsPerPage);
+          commit("SET_LOGS_TOTAL_PAGES", totalPage);
+          commit("SET_LOGS", r.data);
+        })
+        .catch(er => {
+          const notification = {
+            type: "error",
+            message: "An error has ocurred",
+            detail: er
+          };
+          dispatch("notification/add", notification, { root: true });
+        });
+    }
+  },
+  getters: {}
 };
-export const getters = {};
---
+++ src/store/modules/statistics.js
@@ -0,0 +1,30 @@
+import api from "@/services/ApiService";
+
+export default {
+  state: {
+    statistics: []
+  },
+  mutations: {
+    SET_STATISTICS(state, statistics) {
+      state.statistics = statistics;
+    }
+  },
+  actions: {
+    fetchStatistics({ commit, dispatch }) {
+      api
+        .getStatistics()
+        .then(r => {
+          commit("SET_STATISTICS", r.data);
+        })
+        .catch(er => {
+          const notification = {
+            type: "error",
+            message: "An error has ocurred",
+            detail: er
+          };
+          dispatch("notification/add", notification, { root: true });
+        });
+    }
+  },
+  getters: {}
+};
