af05adc1d2a8021de7db9938d8b565a1751397db
Rafael CP <rafael_it@hotmail.com>
2020-04-05 21:00:16 -0300

    feat: add vuex with modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import api from "../services/ApiService";
+import * as error from "../store/modules/errors";
+import * as log from "../store/modules/logs";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    statistics: []
+  },
+  mutations: {
+    SET_STATISTICS(state, statistics) {
+      state.statistics = statistics;
+    }
+  },
+  actions: {
+    fetchStatistics({ commit }) {
+      api
+        .getStatistics()
+        .then(r => {
+          commit("SET_STATISTICS", r.data);
+        })
+        .catch(er => {
+          console.log("An error has ocurred: ", er.response);
+        });
+    }
+  },
+  getters: {},
+  modules: {
+    error,
+    log
+  }
 });
