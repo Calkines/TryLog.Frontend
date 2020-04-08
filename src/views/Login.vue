92d313c3fafe4c0a8394607f980b4de5b26a975e
Rafael CP <rafael_it@hotmail.com>
2020-04-07 23:20:24 -0300

    feat: adiciona funcionalidade de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -13,6 +13,7 @@
                 label="Usuário"
                 :rules="[rules.email]"
                 prepend-icon="fas fa-user-tag"
+                v-model="email"
               ></v-text-field>
               <v-text-field
                 :type="showPassword ? 'text' : 'password'"
@@ -20,17 +21,16 @@
                 prepend-icon="fas fa-lock"
                 :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                 @click:append="showPassword = !showPassword"
+                v-model="password"
               ></v-text-field>
             </v-form>
           </v-card-text>
           <v-divider></v-divider>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="success" :to="'/signup'" x-small rounded
-              >Registrar</v-btn
-            >
+            <v-btn color="success" :to="'/signup'" x-small rounded>Registrar</v-btn>
             <v-spacer></v-spacer>
-            <v-btn color="info" x-small rounded>Acessar</v-btn>
+            <v-btn color="info" @click.prevent="login" x-small rounded>Acessar</v-btn>
             <v-spacer></v-spacer>
           </v-card-actions>
           <v-card-actions>
@@ -48,6 +48,8 @@ export default {
     return {
       showPassword: false,
       isLoading: false,
+      email: "",
+      password: "",
       rules: {
         email: v =>
           /^([a-zA-Z0-9_\-\\.]+)@([a-zA-Z0-9_\-\\.]+)\.([a-zA-Z]{2,5})$/g.test(
@@ -64,6 +66,19 @@ export default {
         required: v => !!v || "Este campo é necessário"
       }
     };
+  },
+  methods: {
+    login() {
+      const userObj = {
+        email: this.email,
+        password: this.password
+      };
+      this.$store.dispatch("realizarLogin", { user: userObj }).then(() => {
+        setTimeout(() => {
+          this.$router.push({ name: "Home" });
+        }, 500);
+      });
+    }
   }
 };
 </script>
