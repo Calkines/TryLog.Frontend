d738f67e2e0ef67ef312449a17c6b8b9c42a2a3f
Rafael CP <rafael_it@hotmail.com>
2020-04-07 23:18:21 -0300

    feat: adiciona rotas de login

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,28 +1,37 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
-  withCredentials: false,
+  baseURL: "http://localhost:3000/",
+  //withCredentials: false,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json"
   }
 });
+apiClient.interceptors.request.use(req => {
+  const keyUser = JSON.parse(localStorage.getItem("user"));
+  const token = keyUser ? keyUser.token : "";
+  req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
 
 export default {
   getStatistics() {
-    return apiClient.get("/statistics");
+    return apiClient.get("api/statistics");
   },
   getErrors() {
-    return apiClient.get("/errors");
+    return apiClient.get("api/errors");
   },
   getLogs() {
-    return apiClient.get("/events");
+    return apiClient.get("api/logs");
   },
   getLogs(itensPerPage, startPage) {
-    return apiClient.get(`/events?_limit=${itensPerPage}&_page=${startPage}`);
+    return apiClient.get(`api/logs?_limit=${itensPerPage}&_page=${startPage}`);
   },
   registerUser(user) {
-    return apiClient.post("/registerUser", user);
+    return apiClient.post("/auth/register", JSON.stringify(user));
+  },
+  realizarLogin(user) {
+    return apiClient.post("/auth/login", JSON.stringify(user));
   }
 };
